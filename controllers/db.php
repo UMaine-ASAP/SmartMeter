415089de0cd9e317defdccd82e6905962938d203
Ross Trundy <ross.trundy@maine.edu>
2012-08-03 11:33:40 -0400

    Added logging system, made DB controller use it to help with future troubleshooting

--- controllers/db.php
+++ controllers/db.php
@@ -5,24 +5,30 @@ class DBController
 
     private $initialized = false;
 
+    /*
+     * If successful, returns a PDO connection object. If none previously existed, it will be created.
+     */
     static function getConnection()
     {
         if (!$this->initialized)
         {
             try
             {
+                $log = new KLogger("../logs");
                 $dbh = new PDO('mysql:host=' . $GLOBALS['HOST'] . ';dbname=' . $GLOBALS['DATABASE'], $GLOBALS['USERNAME'], $GLOBALS['PASSWORD']);
                 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 $this->dbh = $dbh;
 
                 $this->initialized = true;
-
+                $log->logInfo("DBController: Created PDO object");
                 return $dbh;
             } 
             catch (PDOException $ex)
             {
                 error_log($ex);
                 $dbh = null;
+                $log = new KLogger("../logs");
+                $log->logError("DBController: Failed to create PDO object");
                 return false;
             }
         } 
@@ -31,6 +37,7 @@ class DBController
             return $this->dbh;
         }
     }
+    
 }
 
 ?>
