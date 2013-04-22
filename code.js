c20e8852f2c46f5d8d5734d9ddc879add3c38aa7
Sean Lyford <sean.lyford@umit.maine.edu>
2013-04-22 13:47:08 -0400

    implementation of the sidebar, presentation of the windows

--- code.js
+++ code.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 
-
+    var eCost=25;
+    var sRange1=80;
+    var sRange2=20;
 	$( ".sliderRange" ).slider({
 				range: true,
 				min: 0,
@@ -12,7 +14,9 @@ $(document).ready(function(){
 			});
 
 	buttonPush();
-	estimatedCost();
+	estimatedCost(eCost);
+    summaryForTheRange1(sRange1);
+    summaryForTheRange2(sRange2);
 });
 
 var state;
@@ -25,23 +29,78 @@ function buttonPush(){
     })
 }
 
-function estimatedCost(){
-	
-    $('foo').click(function() {
-    	var i = setInterval(function ()
-        {
-            if (currentVal === endVal)
-            {
-                clearInterval(i);
-                reset.disabled = false;
-                $counter.animate({fontSize: fontSize});
-            }
-            else
-            {
-                currentVal++;
-                $counter.text(prefix+currentVal).animate({fontSize: '+=1'}, 100);
-            }
-        }, 100);
 
-    })
+// Show a random number during 5s then display the last one 
+
+function estimatedCost(endVal){
+    var $counter = $('#counter'),
+        startVal = $counter.text(),
+        currentVal,
+        suffix = '$',
+        currentVal = startVal;
+
+    $counter.css('opacity', '.3');
+        
+    var i = setInterval(function (){
+        if (currentVal === endVal){
+            clearInterval(i);
+            $counter.css('opacity', '1');
+        }
+        else{
+            currentVal++;
+            $counter.text(currentVal+" "+suffix);
+        }
+    }, 30);
 }
+
+function summaryForTheRange1(endValue){
+
+    var $percentage = $('#percentageNormal');
+
+    setTimeout(function(){
+        
+        var progressBar = $('#progressBar1');
+        var perc = endValue;
+        var current_perc = 0;
+
+        var progress = setInterval(function() {
+            if (current_perc>=perc) {
+                clearInterval(progress);
+            } else {
+                current_perc ++;
+                progressBar.css('width', (current_perc)+'%');
+            }
+           $percentage.text("Normal (" + (current_perc)+'%):');
+
+        }, 30);
+
+    },30);
+
+}
+
+function summaryForTheRange2(endValue){
+
+    var $percentage = $('#percentagePeak');
+
+    setTimeout(function(){
+        
+        var progressBar = $('#progressBar2');
+        var perc = endValue;
+        var current_perc = 0;
+
+        var progress = setInterval(function() {
+            if (current_perc>=perc) {
+                clearInterval(progress);
+            } else {
+                current_perc ++;
+                progressBar.css('width', (current_perc)+'%');
+            }
+           $percentage.text("Peacks (" + (current_perc)+'%):');
+
+        }, 30);
+
+    },30);
+
+}                                          
+                                        
+
